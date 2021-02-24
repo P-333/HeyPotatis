95c6029fcf8a5f3eec1a5591b73315a7aec698bb
potatisgrizen <potatis@potatisgrizen.com>
2021-02-24 20:13:36 +0100

    :sparkles: Fixed a bug with axios. Meme command temporary disabled

--- src/commands/miscellaneous/meme.ts
+++ src/commands/miscellaneous/meme.ts
@@ -1,39 +1,39 @@
-import {Command} from "../../domain/Command";
-import {Message} from "discord.js";
-import {PGClient} from "../../domain/PGClient";
-import {getRandomMeme} from '@blad3mak3r/reddit-memes';
+// import {Command} from "../../domain/Command";
+// import {Message} from "discord.js";
+// import {PGClient} from "../../domain/PGClient";
+// import {getRandomMeme} from '@blad3mak3r/reddit-memes';
 
 
-class Meme extends Command {
+// class Meme extends Command {
 
-    constructor(client: PGClient) {
-        super(client, {
-            name: "meme",
-            description: "A meme command",
-            usage: ".meme"
-        })
-    }
+//     constructor(client: PGClient) {
+//         super(client, {
+//             name: "meme",
+//             description: "A meme command",
+//             usage: ".meme"
+//         })
+//     }
 
-    run(message: Message, args: Array<string>) {
-        if (args[0]) {
-            try {
-                getRandomMeme(args[0]).then(Meme => {
-                    return message.channel.send(Meme.image);
-                })
-            } catch (err) {
-                this.client.errHandler(message, err);
-            }
-            return
-        }
-        try {
-            getRandomMeme().then(Meme => {
-                return message.channel.send(Meme.image);
-            })
-        } catch (err) {
-            this.client.errHandler(message, err);
-        }
-    }
+//     run(message: Message, args: Array<string>) {
+//         if (args[0]) {
+//             try {
+//                 getRandomMeme(args[0]).then(Meme => {
+//                     return message.channel.send(Meme.image);
+//                 })
+//             } catch (err) {
+//                 this.client.errHandler(message, err);
+//             }
+//             return
+//         }
+//         try {
+//             getRandomMeme().then(Meme => {
+//                 return message.channel.send(Meme.image);
+//             })
+//         } catch (err) {
+//             this.client.errHandler(message, err);
+//         }
+//     }
 
-}
+// }
 
-export = Meme;
+// export = Meme;
