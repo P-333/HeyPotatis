8cf9292e57b9a71f9245ae4c6960914f0f2633ea
potatisgrizen <potatis@potatisgrizen.com>
2021-02-26 04:36:48 +0100

    :sparkes: Changes in Pause command

--- src/MusicUtil/MusicFunctions/Pause.ts
+++ src/MusicUtil/MusicFunctions/Pause.ts
@@ -3,6 +3,8 @@ import { nothingPlayingEmbed } from '../MusicQueue';
 
 export default function pause(message: any, serverQueue:any) {
 
+  if (!serverQueue) return message.channel.send(nothingPlayingEmbed);
+
   const pausedEmbed = new MessageEmbed()
     .setTitle('⏸ | I have paused the queue.')
     .setColor('GREEN')
@@ -14,7 +16,6 @@ export default function pause(message: any, serverQueue:any) {
     .setColor('GREEN')
     .setDescription(`**The queue is paused on song:** [${serverQueue.songs[0].title}](${serverQueue.songs[0].url}) | \`${serverQueue.songs[0].duration} Requested by: ${serverQueue.songs[0].req.tag}\``)
     .setFooter('Hey Potatis - 2020');
-  if (!serverQueue) return message.channel.send(nothingPlayingEmbed);
   if (!serverQueue.playing) return message.channel.send(alreadyPausedEmbed);
   serverQueue.playing = false;
   serverQueue.connection.dispatcher.pause();
